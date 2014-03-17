7d7681656f2f87d49e0ca1c7daf882d493162da1
danielspector <danielyspector@gmail.com>
2014-03-17 18:59:30 -0400

    attempted to add validation for dates

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '4.0.2'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
-
+gem 'validates_timeliness', '~> 3.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,8 +2,17 @@ class Auction < ActiveRecord::Base
 
   has_many :bids, dependent: :destroy
   belongs_to :seller, class_name: "User"
-
+  validates_uniqueness_of :title
+  # validate :not_ended?
+  # validates_date :end_time, :on_or_before => lambda { Time.now }
+  
   def price_in_dollars
     self.price = price/100.00
+  end
+
+  def not_ended?
+    if self.end_time && (self.end_time >= Chronic.parse("March 17, 2014"))
+      errors.add(:end_time, "Can't be in the past")
+    end
   end
 end
