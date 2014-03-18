3081e5cdb371e32b81fa2344706badebc8cc89da
danielspector <danielyspector@gmail.com>
2014-03-18 10:20:10 -0400

    passed all part 1 controller specs

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -2,13 +2,8 @@ class BidsController < ApplicationController
   def create
     @auction = Auction.find(params[:auction_id])
     @bid = @auction.bids.build(amount: params[:amount], bidder_id: params[:bidder_id])
-    binding.pry
-    if @auction.bids.length == 0
-      @bid.save
-      redirect_to auction_path(@auction)
-    end
-
-    if params[:amount].to_i > @auction.highest_bid.amount
+    # binding.pry
+    if params[:amount].to_i > @auction.highest_bid.amount_in_dollars
       if @bid.save
         redirect_to auction_path(@auction)
       else
